9b51cab95e94f7ea05a3603801d8a79773732fdf
Joe Lennon <joe@array.ie>
2017-03-13 09:44:48 +0000

    Form styling improvements.

--- resources/views/blog/themes/default/_partials/form.blade.php
+++ resources/views/blog/themes/default/_partials/form.blade.php
@@ -1,14 +1,16 @@
-@if(session()->has('status'))
-	<div class="alert alert-success">{{ session('status') }}</div>
-@else
-	@if($errors->first())
-		<div class="alert alert-danger">@lang('app.form_error')</div>
-	@endif
+<div class="post-form">
+	@if(session()->has('status'))
+		<div class="alert alert-success">{{ session('status') }}</div>
+	@else
+		@if($errors->first())
+			<div class="alert alert-danger">@lang('app.form_error')</div>
+		@endif
 
-	{!! BootForm::openHorizontal(['sm' => [2, 10]]) !!}
-		{!! BootForm::text(trans('blog.form.name'), 'name')->autofocus() !!}
-		{!! BootForm::email(trans('blog.form.email'), 'email')->required() !!}
-		{!! BootForm::textarea(trans('blog.form.message'), 'message')->required() !!}
-		{!! BootForm::submit(trans('blog.form.send'))->addClass('btn-primary') !!}
-	{!! BootForm::close() !!}
-@endif
+		{!! BootForm::openHorizontal(['sm' => [3, 9]]) !!}
+			{!! BootForm::text(trans('blog.form.name'), 'name')->autofocus() !!}
+			{!! BootForm::email(trans('blog.form.email'), 'email')->required() !!}
+			{!! BootForm::textarea(trans('blog.form.message'), 'message')->required() !!}
+			{!! BootForm::submit(trans('blog.form.send'))->addClass('btn-primary') !!}
+		{!! BootForm::close() !!}
+	@endif
+</div>
